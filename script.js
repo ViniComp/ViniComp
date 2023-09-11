31e0fc244f85988fe3488f33691236901661ae04
Vinicius Oliveira <96297937+ViniComp@users.noreply.github.com>
2023-09-11 00:21:50 -0300

    commit inicial
    Primeiro commit

---
+++ script.js
@@ -0,0 +1,16 @@
+const btn = document.getElementById("btn-topo")
+btn.addEventListener("click", function(){
+    window.scrollTo(0,0)
+})
+
+document.addEventListener('scroll',ocultar)
+
+function ocultar(){
+    if(window.scrollY > 10){
+        btn.style.display = "flex"
+    }else{
+        btn.style.display = "none"
+    }
+}
+
+ocultar()
